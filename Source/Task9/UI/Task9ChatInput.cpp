4ea9579a6ec460d7c2c653723d7b25bda37c9581
Joung_Juhyeon <wjdwngus2056@gmailc.om>
2026-03-17 03:39:38 +0900

    [Add] Chat UI Widget C++ 생성
    .Build.cs에 UMG, Slate, SlateCore 모듈 의존성 추가
    
    Task9ChatInput 추가

---
+++ Source/Task9/UI/Task9ChatInput.cpp
@@ -0,0 +1,1 @@
+﻿#include "Task9ChatInput.h"
---
+++ Source/Task9/UI/Task9ChatInput.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Task9ChatInput.generated.h"
+
+class UEditableTextBox;
+
+UCLASS()
+class TASK9_API UTask9ChatInput : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UEditableTextBox> ChatInput;
+};
